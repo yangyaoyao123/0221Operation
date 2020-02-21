2782147c0abd3fcf50e73563fa2d205c7be93064
yyy <156515159@qq.com>
2020-02-21 20:05:46 +0800

    This is IDEA write java class

--- 0221Operation/src/com/yyy/operation/Test.java
+++ 0221Operation/src/com/yyy/operation/Test.java
@@ -2,6 +2,6 @@ package com.yyy.operation;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello Yyy,这是你的IDEA操作");
+        System.out.println("Hello Yyy,这是你的IDEA操作!");
     }
 }
