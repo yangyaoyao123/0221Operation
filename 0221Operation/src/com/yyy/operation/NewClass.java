b73181cd26b875436a20f185bb2bf2a4e4975d06
yyy <156515159@qq.com>
2020-02-21 21:51:19 +0800

    This is the class created by IDEA

--- 0221Operation/src/com/yyy/operation/NewClass.java
+++ 0221Operation/src/com/yyy/operation/NewClass.java
@@ -2,6 +2,6 @@ package com.yyy.operation;
 
 public class NewClass {
     public static void main(String[] args) {
-        System.out.println("创建的新的");
+        System.out.println("创建的新的类");
     }
 }
